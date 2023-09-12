c7e0f8f7fd7da693e101ad53d75f454ff161420f
QveczekDev <rdud1471@gmail.com>
2023-09-12 11:21:19 +0200

    programowanie obiektowe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+            public static void main(String[] args) {
+                System.out.println("Hello world!");
+
+                Osoba osoba = new Osoba();
+             osoba.setImie("Jan");
+                System.out.println("Imie naszej osoby" + osoba.getImie());
+                osoba.setWiek(-20);
+                System.out.println("Wiek" + osoba.getWiek());
+            }
+        }
+
---
+++ src/Osoba.java
@@ -0,0 +1,43 @@
+
+
+    public class Osoba {
+        private String imie;
+        private String nazwisko;
+        private int wiek;
+
+        public String getImie() {
+            return imie;
+        }
+
+        public void setImie(String imie) {
+           if(imie.length()<3)
+               System.out.println("Imie musi mieć co najmniej 3 znaki");
+           else{
+               this.imie = imie;
+           }
+
+        }
+
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public void setNazwisko(String nazwisko) {
+            this.nazwisko = nazwisko;
+        }
+
+        public int getWiek() {
+            return wiek;
+        }
+
+        public void setWiek(int wiek) {
+       if(wiek<0){
+           this.wiek = 0;
+       }
+       else{
+this.wiek = wiek;
+       }
+        }
+    }
+
+

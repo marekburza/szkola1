45df772e5821ea0b0f38b0bc5899476c3d18d4e5
QveczekDev <rdud1471@gmail.com>
2023-09-12 12:20:16 +0200

    Szkola/nazuczyciel

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+      /*  Osoba osoba = new Osoba();
+        osoba.setImie("Jan");
+        System.out.println("Imie naszej osoby "+osoba.getImie());
+        osoba.setWiek(-20);
+        System.out.println("Wiek "+osoba.getWiek());
 
-            public static void main(String[] args) {
-                System.out.println("Hello world!");
-
-                Osoba osoba = new Osoba();
-             osoba.setImie("Jan");
-                System.out.println("Imie naszej osoby" + osoba.getImie());
-                osoba.setWiek(-20);
-                System.out.println("Wiek" + osoba.getWiek());
-            }
-        }
-
+        Osoba osoba2 = new Osoba("Ola","Starsza",19);
+        System.out.println(osoba2);
+*/
+        Uczen uczen1P1 = new Uczen("Emil","Smalandzki",12);
+        Uczen uczen1P2 = new Uczen("Emilia","Pragmatyczna",9);
+        System.out.println(uczen1P2);
+        System.out.println(uczen1P1);
+        Nauczyciel SN = new Nauczyciel("Sebastian","Nadwiślański","polski");
+        System.out.println(SN);
+    }
+}
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,43 +1,69 @@
+public class Osoba {
+    private String imie;
+    private String nazwisko;
+    private int wiek;
 
+    public Osoba() {
+        this.imie = "Edward";
+        this.nazwisko = "Nowak";
+        this.wiek = 30;
+        //konstruktor z wartościami domyślnymi
+    }
 
-    public class Osoba {
-        private String imie;
-        private String nazwisko;
-        private int wiek;
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek =7;
+    }
 
-        public String getImie() {
-            return imie;
-        }
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
 
-        public void setImie(String imie) {
-           if(imie.length()<3)
-               System.out.println("Imie musi mieć co najmniej 3 znaki");
-           else{
-               this.imie = imie;
-           }
+    public String getImie() {
+        return imie;
+    }
 
-        }
-
-        public String getNazwisko() {
-            return nazwisko;
-        }
-
-        public void setNazwisko(String nazwisko) {
-            this.nazwisko = nazwisko;
-        }
-
-        public int getWiek() {
-            return wiek;
-        }
-
-        public void setWiek(int wiek) {
-       if(wiek<0){
-           this.wiek = 0;
-       }
-       else{
-this.wiek = wiek;
-       }
+    public void setImie(String imie) {
+        if(imie.length()<3)
+            System.out.println("Imie musi mieć co najmniej 3 znaki");
+        else {
+            //TODO: sprawdzić czy tylko litery
+            //TODO: pierwsza litera duża pozostałe małe
+            this.imie = imie;
         }
     }
 
+    public String getNazwisko() {
+        return nazwisko;
+    }
 
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        if(wiek<0){
+            this.wiek =0;
+        }
+        else{
+            this.wiek = wiek;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba {" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", wiek=" + wiek +
+                '}';
+    }
+}
